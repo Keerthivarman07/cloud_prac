8420bd03af16b4b961d704ec5bc6e85d80cabb53
Keerthivarman S <keerthivarman010@gmail.com>
2026-04-27 14:27:10 +0530

    Initial commit

--- binary.py
+++ binary.py
@@ -2,5 +2,6 @@ a=input("Enter a binary number: ")
 b=input("Enter another binary number: ")
 a=int(a,2)
 b=int(b,2)
-c=bin(a+b)
-print("The sum of the two binary numbers is: ", c[2:])
+c=a+b
+c=bin(c)[2:]
+print("The sum of two binary numbers are: ", c)
